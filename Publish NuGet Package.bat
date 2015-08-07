cc6a1f02362128d90767e96f3eb4776c1b7648d6
Adam <adam@adamslair.net>
2015-08-07 17:35:29 +0200

    :sunrise: Initial Duality Adjustments #CHANGE: Removed signing and keys. #ADD: Added NuGet publishing script for Duality dependency packages.

---
+++ Publish NuGet Package.bat
@@ -0,0 +1,23 @@
+:: search for msbuild and make it available
+set bb.build.msbuild.exe=
+for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set msbuildPath=%%D
+set PATH=%PATH%;%msbuildPath%
+
+:: make sure we have a clean release build
+msbuild /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU" OpenTK.sln
+
+:: remove any existing nupkg files
+del *.nupkg
+
+:: build the nuget packages
+.nuget\nuget pack Source\OpenTK\OpenTK.csproj -Properties Configuration=Release
+.nuget\nuget pack Source\GLControl\OpenTK.GLControl.csproj -Properties Configuration=Release
+
+:: upload the nuget packages
+.nuget\nuget push AdamsLair.OpenTK.*.nupkg
+.nuget\nuget push AdamsLair.OpenTK.GLControl.*.nupkg
+
+:: remove nupkg files after uploading them
+del *.nupkg
+
+pause
